7f9e12a56b7b1637c60297df303ab02a378239c5
Tech-Doc <dr.stanleyperpetual@gmail.com>
2023-11-06 16:01:47 -0800

    jquery

--- 0x15-javascript-web_jquery/0-script.js
+++ 0x15-javascript-web_jquery/0-script.js
@@ -1,9 +1,3 @@
-// Wait for the DOM to be fully loaded before manipulating it
-document.addEventListener("DOMContentLoaded", function () {
-  // Get the <header> element using document.querySelector
-  var headerElement = document.querySelector("header");
+const header = document.querySelector('header');
 
-  // Set the text color to red (#FF0000)
-  headerElement.style.color = "#FF0000";
-});
-
+header.style.color = '#FF0000';
